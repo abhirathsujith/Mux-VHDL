8d557f847c827e46edd3d19db7278f9e612e6e6c
Abhirath Sujith <78019581+abhirathsujith@users.noreply.github.com>
2022-10-19 23:16:02 +0530

    Update mux.vhd

--- mux.vhd
+++ mux.vhd
@@ -1,15 +1,24 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
+
+
 entity mux is
 port (A,B,C,D: in std_logic; S0,S1: in std_logic; Z: out std_logic);
 end mux;
 
-architecture MUX1 of mux is
+architecture Behavioral of muxb is
 
 begin
 
-Z <= ((not S1) and (not S0) and ( A )) or ((not S1) and ( S0) and ( B )) or (( S1) and (not S0) and ( C )) or ((S1) and (S0) and ( D ));
+process (A,B,C,D,S0,S1)
+begin
 
+if (S1='0' and S0='0') then Z<=A;
+elsif (S1='0' and S0='1') then Z<=B;
+elsif (S1='1' and S0='0') then Z<=C;
+else Z<=D;
 
-end MUX1;
+end if;
+end process;
+end Behavioral;
